3735a0506720999208c5888bc711e576c2e8db2a
Varadharajan <srinivasan.varadharajan1@t-mobile.com>
2021-08-12 16:54:57 -0400

    Library service: Add book, get book list, delete all books 2.0

--- src/main/java/com/galvanize/tmo/paspringstarter/dto/BookDTO.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/dto/BookDTO.java
@@ -1,15 +1,10 @@
 package com.galvanize.tmo.paspringstarter.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import org.springframework.core.style.ToStringCreator;
-import org.springframework.core.style.ToStringStyler;
 
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
 public class BookDTO implements Serializable {
 
     private String author;
